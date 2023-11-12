881569b77a94a3fdca2fdac904e7ff4f637e9ef1
Nadezhda Kuklina <nadezhda.kuklina@bimeister.com>
2023-11-12 22:34:43 +0300

    Add project files.

---
+++ addressbook_tests_white/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("addressbook_tests_white")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("addressbook_tests_white")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2fb63d93-a504-4598-b151-ee81076b32a7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ addressbook_tests_white/appmanager/ApplicationManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestStack.White;
+using TestStack.White.UIItems.WindowItems;
+using TestStack.White.UIItems;
+
+
+namespace addressbook_tests_white
+{
+    public class ApplicationManager
+    {
+        public static string WINTITLE = "Free Address Book";
+        
+        private GroupHelper groupHelper;
+
+        public ApplicationManager()
+        {
+            Application app = Application.Launch(@"C:\07. Курсы\Программирование на C# для тестировщиков\Занятие 8. Работа с Windows GUI\FreeAddressBookPortable\AddressBook.exe");
+
+            MainWindow = app.GetWindow(WINTITLE);
+
+            groupHelper = new GroupHelper(this);
+        }
+
+        public void Stop()
+        {
+            MainWindow.Get<Button>("uxExitAddressButton").Click();            
+        }
+
+        public Window MainWindow { get; private set; }
+
+
+        public GroupHelper Groups
+        {
+            get
+            {
+                return groupHelper;
+            }
+        }
+    }
+    
+}
